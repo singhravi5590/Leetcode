7725da374116fe9158f671ca420a77569202f8b2
singhravi5590 <singhravi5590@gmail.com>
2025-07-27 19:39:48 +0530

    Time: 16 ms (90.95%) | Memory: 55.9 MB (82.54%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let start = 0;
+    let end = 0;
+
+    function expandAroundCenter(left, right){
+        while(left >= 0 && right < s.length && s[left] === s[right]){
+            left --;
+            right++;
+        }
+
+        return right - left - 1
+    }
+
+    for(let i = 0; i<s.length; i++){
+        let len1 = expandAroundCenter(i, i);
+        let len2 = expandAroundCenter(i, i+1);
+        let len = Math.max(len1, len2);
+
+        if(len > end - start){
+            start = i - Math.floor((len-1)/2);
+            end = i + Math.floor((len/2))
+        }
+    }
+
+    return s.substring(start, end+1);
+};
